afdc86a66777ba7764619abbd826279cbbf42ed0
Defender <1303360101@qq.com>
2023-09-21 10:57:46 +0800

    提交server.go

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+func handler22(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hel12312lo World, %s!", request.URL.Path[1:])
+}
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/2", handler22)
+	http.ListenAndServe(":8080", nil)
+}
